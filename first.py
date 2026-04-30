4202eb9ea77b263c3131676b3108904b20a2527b
nazakatalikhan991-ctrl <nazakatalikhan991@gmail.com>
2026-04-30 20:20:02 +0500

    first.py

---
+++ first.py
@@ -0,0 +1,8 @@
+class object:
+            def __init__(self,name,age):
+                   self.name = name
+                   self.age = age
+             def show(self):
+                   print("Student name is :" ,self.name, "\nAnd age is:", self.age)
+rel = object("Agha Khan", 34)
+rel.show()
